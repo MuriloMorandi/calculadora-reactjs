b52dcac18a925dd54bfd03049c54325e6d96102f
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-05-31 17:08:18 -0300

    persistência de dados do histórico de operações

--- src/contexts/CalculatorContext.jsx
+++ src/contexts/CalculatorContext.jsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react";
 
 export const CalculatorContext = createContext();
 
 export const CalculatorProvider = ({ children }) => {
     const [history, setHistory] = useState([]);
-
+    const historyStorageKey = 'history';
+    
     const updateHistory = (operation, result) => {
-        setHistory((prev) => [...prev, `${operation}=${result}`])
+        setHistory((prev) => {
+            const updateHistory = [...prev, `${operation}=${result}`];
+            localStorage.setItem(
+                historyStorageKey,
+                JSON.stringify(updateHistory)
+            );
+
+            return updateHistory;
+        })
     }
+
+    useEffect(() => {
+        const savedHistory = localStorage.getItem(historyStorageKey);
+        if (savedHistory) {
+            setHistory(JSON.parse(savedHistory));
+        }
+    }, [])
 
     return (
         <CalculatorContext.Provider
